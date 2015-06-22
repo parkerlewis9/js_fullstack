b9c04c51fd80ca1e4d94ea9a25a09941d63e1985
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-06-22 14:50:33 -0700

    successful requests for data

--- public/js/playersNewScript.js
+++ public/js/playersNewScript.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
 
+
+
 	$("#player1-form").on("submit", function(e) {
 		e.preventDefault();
-		console.log("submitted")
+		var firstName = $("#player1first").val();
+		var lastName = $("#player1last").val();
+		var url = "https://probasketballapi.com/players?api_key=SoBO1Is4dA2cpkR0J9mlMgbftZv5wzNu&first_name=" + firstName + "&last_name=" + lastName;
+//First request to get id:
+		$.post( url , function( data ) {
+		  var data = JSON.parse(data)
+		  var player_id = data[0].player_id;
+		  var newUrl = "https://probasketballapi.com/stats/players?api_key=SoBO1Is4dA2cpkR0J9mlMgbftZv5wzNu&player_id=" + player_id;
+//Request to get data:
+		  $.post( newUrl , function( data ) {
+		    console.log(data)
+		  });	
+		});
+
 	})
+
+	//TODO --- Make one function to search for results
 
 })
