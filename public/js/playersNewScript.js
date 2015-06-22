3d32c95f25e035d3f901f9f05877d1b4d6fa0585
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-06-22 15:33:22 -0700

    stats appear on page after ajax call

--- public/js/playersNewScript.js
+++ public/js/playersNewScript.js
@@ -1,25 +1,81 @@
 $(document).ready(function() {
+var stats;
+
+
+	function lookUpPlayerStats(firstName, lastName, callback) {
+		$("#errorMsg").html("")
+		var url = "https://probasketballapi.com/players?api_key=SoBO1Is4dA2cpkR0J9mlMgbftZv5wzNu&first_name=" + firstName + "&last_name=" + lastName;
+		//First request to get id:
+		$.post( url , function( data ) {
+		  var playerData = JSON.parse(data)
+	//If we get something back:
+		  if(playerData[0]) {
+		  	var player_id = playerData[0].player_id;
+		  	var newUrl = "https://probasketballapi.com/stats/players?api_key=SoBO1Is4dA2cpkR0J9mlMgbftZv5wzNu&player_id=" + player_id;
+			//Request to get data:
+		  	$.post( newUrl , function( data ) {
+		   		 var playerStats = JSON.parse(data);
+					var points = 0;
+					var blocks = 0;
+					var steals = 0;
+					var assists = 0;
+					var rebounds = 0;
+					playerStats.forEach(function(datum) {
+						points += datum.box_pts;
+						blocks += datum.box_blk;
+						steals += datum.box_stl;
+						assists += datum.box_ast;
+						rebounds += (datum.box_oreb + datum.box_dreb)
+					});
+					var score = points + blocks + steals + assists + rebounds;
+					//Object to send out
+					stats = {};
+					stats.points = points;
+					stats.blocks = blocks;
+					stats.steals = steals
+					stats.assists = assists;
+					stats.rebounds = rebounds;
+					stats.score = score;
+					return callback(stats)
+		 	 });
+	//If not: 	
+		  } else {
+		  	$("#player1-form").append('<p id="errorMsg">Please try again. Check your spelling. (If it is correct we may not have that player on file.)</p>');
+		  }
+		});
+	}
 
 
 
+//Event listener for one player look up
 	$("#player1-form").on("submit", function(e) {
 		e.preventDefault();
 		var firstName = $("#player1first").val();
 		var lastName = $("#player1last").val();
-		var url = "https://probasketballapi.com/players?api_key=SoBO1Is4dA2cpkR0J9mlMgbftZv5wzNu&first_name=" + firstName + "&last_name=" + lastName;
-//First request to get id:
-		$.post( url , function( data ) {
-		  var data = JSON.parse(data)
-		  var player_id = data[0].player_id;
-		  var newUrl = "https://probasketballapi.com/stats/players?api_key=SoBO1Is4dA2cpkR0J9mlMgbftZv5wzNu&player_id=" + player_id;
-//Request to get data:
-		  $.post( newUrl , function( data ) {
-		    console.log(data)
-		  });	
-		});
+		lookUpPlayerStats(firstName, lastName, function(stats) {
+			$("#pointsplayer1").html(stats.points)
+			$("#blocksplayer1").html(stats.blocks)
+			$("#assistsplayer1").html(stats.assists)
+			$("#reboundsplayer1").html(stats.rebounds)
+			$("#stealsplayer1").html(stats.steals)
+			$("#scoreplayer1").html(stats.score)
+		})
+			
+		
 
 	})
 
-	//TODO --- Make one function to search for results
+	//TODO --- Make one function to search for results  √
 
 })
+
+
+
+
+
+
+
+
+
+
+
