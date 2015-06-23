059910ef2530834888fa79d99752a55506e225bb
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-06-23 08:58:01 -0700

    new player div on new team page and still can look up player

--- public/js/playersNewScript.js
+++ public/js/playersNewScript.js
@@ -61,32 +61,23 @@ var stats;
 					jsonp: "callback",
 					dataType: "jsonp"
 				}).done(function(data) {
-				console.log(data.responseData.results[0].unescapedUrl);
 				var imgUrl = data.responseData.results[0].unescapedUrl;
+
 				//Add image to page
 				$("#urlplayer1").attr("src", imgUrl)
-				$("#player1image").attr("value", imgUrl)
 
 				//Add stats to page
 				$("#pointsplayer1").html(stats.points)
-				$("#player1points").attr("value", stats.points)
 
 				$("#blocksplayer1").html(stats.blocks)
-				$("#player1blocks").attr("value", stats.blocks)
 
 				$("#assistsplayer1").html(stats.assists)
-				$("#player1assists").attr("value", stats.assists)
 
 				$("#reboundsplayer1").html(stats.rebounds)
-				$("#player1rebounds").attr("value", stats.rebounds);
 
 				$("#stealsplayer1").html(stats.steals)
-				$("#player1steals").attr("value", stats.steals)
 
 				$("#scoreplayer1").html(stats.score)
-				$("#player1score").attr("value", stats.score)
-
-				$("#player1name").attr("value", firstName + " " + lastName)
 
 				//Add name to page
 				$("#nameplayer1").html(firstName + " " + lastName)
