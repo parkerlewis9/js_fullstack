97d48c1f19b60091b311101444ddc2ca72d2d7be
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-06-24 16:18:45 -0700

    got chart on page-need data to get to it

---
+++ public/js/playersShowScript.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+
+//Ajax call to get data
+	$.ajax({
+			url: window.location.pathname
+			method: "GET",
+			dataType: "json"
+		}).done(function(data) {
+			console.log(data)
+		})
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+	var ctx = $("#myChart").get(0).getContext("2d");
+
+	function makeBarGraph(callback) {
+		data.datasets[0].data.push(20);
+		return callback();
+	}
+
+
+	var data = {
+	    labels: ["Points", "Assists", "Rebounds", "Steals", "Blocks"],
+	    datasets: [
+	        {
+	            label: "Player's Stats",
+	            fillColor: "rgba(220,220,220,0.5)",
+	            strokeColor: "rgba(220,220,220,0.8)",
+	            highlightFill: "rgba(220,220,220,0.75)",
+	            highlightStroke: "rgba(220,220,220,1)",
+	            data: [59, 80, 81, 56]
+	        }
+	    ]
+	};
+
+	var options = {
+				    //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+				    scaleBeginAtZero : true,
+
+				    //Boolean - Whether grid lines are shown across the chart
+				    scaleShowGridLines : true,
+
+				    //String - Colour of the grid lines
+				    scaleGridLineColor : "rgba(0,0,0,.05)",
+
+				    //Number - Width of the grid lines
+				    scaleGridLineWidth : 1,
+
+				    //Boolean - Whether to show horizontal lines (except X axis)
+				    scaleShowHorizontalLines: true,
+
+				    //Boolean - Whether to show vertical lines (except Y axis)
+				    scaleShowVerticalLines: true,
+
+				    //Boolean - If there is a stroke on each bar
+				    barShowStroke : true,
+
+				    //Number - Pixel width of the bar stroke
+				    barStrokeWidth : 2,
+
+				    //Number - Spacing between each of the X value sets
+				    barValueSpacing : 5,
+
+				    //Number - Spacing between data sets within X values
+				    barDatasetSpacing : 1,
+
+				    //String - A legend template
+				    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+				}
+	makeBarGraph(function() {
+		var myBarChart = new Chart(ctx).Bar(data, options);	
+	})
+	
+
+
+
+})
